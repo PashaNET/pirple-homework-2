0e132a2331340b97e67014ea59281832e56d3d49
PavelNET <pavlo.netrebsky@gmail.com>
2018-02-13 11:14:52 +0200

    Test config

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/pattern', pattern);
+app.use('/pattern', pattern);//didn't work with 'route'
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
