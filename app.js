709c4f9c47d38cc7f3bbc65f88bc397a84d291be
PashaNET <pavlo.netrebsky@gmail.com>
2018-09-08 11:34:35 +0300

    query object, headers

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-  let parsedUrl = url.parse(req.url);
-  console.log(parsedUrl.path);
-  res.end('Just test\n');
+  let parsedUrl = url.parse(req.url, true);
+  
+  let queryStringObj = parsedUrl.query;
+  
+  let method = req.method.toLocaleLowerCase();
+
+  let headersObj = req.headers;
+  
+  res.end('Just test ' + method + '\n');
+
+  console.log(headersObj);
 });
 
 server.listen(3001, () => {
