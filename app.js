474c8c873451a45232f3dfeb0e6d9abe399a2c81
PashaNET <pavlo.netrebsky@gmail.com>
2018-09-08 13:05:06 +0300

    payload handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 //Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req, res) => {
   let parsedUrl = url.parse(req.url, true);
@@ -14,10 +15,19 @@ const server = http.createServer((req, res) => {
   let method = req.method.toLocaleLowerCase();
 
   let headersObj = req.headers;
-  
-  res.end('Just test ' + method + '\n');
 
-  console.log(headersObj);
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+    console.log('line-->' + buffer + '\n');
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+    // console.log(headersObj);
+    res.end('Payload text --> ');// + buffer + '\n');
+  });
+  
 });
 
 server.listen(3001, () => {
