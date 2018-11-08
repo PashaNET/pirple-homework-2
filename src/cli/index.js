898b090aecc277f677ff74a9a773acf177f52c31
PashaNET <pavlo.netrebsky@gmail.com>
2018-11-08 23:32:44 +0200

    cli, 'help' command

--- src/cli/index.js
+++ src/cli/index.js
@@ -9,11 +9,46 @@
     //  debug = require('debug'),
      events = require('events');
 
-class Events extends events{
-
-}
+ class Events extends events {}
 
  let ev = new Events();
+ 
+ ev.on('menu', (str) => {
+    process.exit(0);
+ });
+
+ ev.on('orders', (str) => {
+    process.exit(0);
+ });
+
+ ev.on('order', (str) => {
+    process.exit(0);
+ });
+
+ ev.on('users', (str) => {
+    process.exit(0);
+ });
+
+ ev.on('user', (str) => {
+    process.exit(0);
+ });
+
+ ev.on('help', (str) => {
+    let commandsDescription = {
+        'menu': 'View all menu items',
+        'orders --{time}': 'View orders placed in {time} hours', 
+        'order --{id}': 'Lookup the details of a specific order by it ID', 
+        'users --{time}': 'View users who have signed up in the last {time} hours', 
+        'user --{email}': 'Lookup the details of a specific user by email address', 
+        'exit': 'Terminate CLI and server'
+    };
+
+    
+ });
+
+ ev.on('exit', (str) => {
+    process.exit(0);
+ });
 
  //Container for cli tasks
  let cli = {};
@@ -48,17 +83,18 @@ class Events extends events{
     if(isValidInput){
         let cliCommands = [
             'menu',
-            'orders', //past 24 hours
-            'order', //details of a specific order
-            'users', //signed upt for last 24 hours
-            'user' //details of a specific user
+            'orders',
+            'order',
+            'users',
+            'user',
+            'help',
+            'exit'
         ];
-
         let command = input.trim().toLowerCase(),
             commandIndex = cliCommands.indexOf(command);
         //perform action if command exist in commands list
         if(commandIndex > -1) {
-            ev.emit(commandIndex, input);
+            ev.emit(input, commandIndex);
         } else {
             console.log(input + ' - is not recognizable command');
             console.log('Please, try again!');
